bed8ff265ca2c5551e27d3a28e88a5762addf6a1
tahsinaltabib <nabilarah33@gmail.com>
2025-07-02 08:49:45 +0600

    create a clock using javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+let hrs = document.getElementById("hrs")
+    let min = document.getElementById("min")
+    let sec = document.getElementById("sec")
+
+    let currentTime = new Data();
+
+    hrs.innerHTML = currentTime.getHours();
+    min.innerHTML = currentTime.getMinutes();
+    sec.innerHTML = currentTime.getSeconds();
